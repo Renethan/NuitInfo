39540e2a16a5cb6a93e4ea6c15fb2ed68ec9d9d8
Jolan RAMBEAUX <jolan.rambeaux@etu.univ-amu.fr>
2025-12-05 05:55:02 +0100

    Implement Konami code redirect functionality

---
+++ scrip_presentation_groupe.js
@@ -0,0 +1,38 @@
+var konamiCode = [
+    'ArrowUp',
+    'ArrowUp',
+    'ArrowDown',
+    'ArrowDown',
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowLeft',
+    'ArrowRight',
+    'b',
+    'a'
+];
+
+var userInput = [];
+var redirectUrl = 'snake.html';
+
+document.addEventListener('keydown', function(e) {
+    userInput.push(e.key);
+
+    if (userInput.length > konamiCode.length) {
+        userInput.shift();
+    }
+
+    if (userInput.length === konamiCode.length) {
+        var isKonami = true;
+
+        for (var i = 0; i < konamiCode.length; i++) {
+            if (konamiCode[i] !== userInput[i]) {
+                isKonami = false;
+                break;
+            }
+        }
+
+        if (isKonami) {
+            window.location.href = redirectUrl;
+        }
+    }
+});
