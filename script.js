c40030e2110a00ecd87ea7a92167a7db7f10a22a
Jolan RAMBEAUX <jolan.rambeaux@etu.univ-amu.fr>
2025-12-05 01:01:27 +0100

    Change snake movement speed to half for arrow keyssdf
    Adjusted snake movement speed for arrow key inputs.

--- script.js
+++ script.js
@@ -92,21 +92,21 @@ function changeDirection(e) {
         // If up arrow key pressed with this condition...
         // snake will not move in the opposite direction
         speedX = 0;
-        speedY = -1;
+        speedY = -0.5;
     }
     else if (e.code == "ArrowDown" && speedY != -1) {
         //If down arrow key pressed
         speedX = 0;
-        speedY = 1;
+        speedY = 0.5;
     }
     else if (e.code == "ArrowLeft" && speedX != 1) {
         //If left arrow key pressed
-        speedX = -1;
+        speedX = -0.5;
         speedY = 0;
     }
     else if (e.code == "ArrowRight" && speedX != -1) { 
         //If Right arrow key pressed
-        speedX = 1;
+        speedX = 0.5;
         speedY = 0;
     }
 }
